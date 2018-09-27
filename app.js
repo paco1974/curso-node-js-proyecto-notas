9d7fc0166d4258bb426b89c99f2e034d9dc570ea
paco <domene.moros@gmail.com>
2018-09-27 19:52:24 +0200

    implementados metodos usando yargs

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 console.log('Aplicación de notas arrancada.')
-const notas = require('./notes')
+const notes = require('./notes')
 
-notas.addNote()
-notas.deleteNote()
+// const yargs = require('yargs')
+// const argv= yargs.argv
+
+const { argv } = require('yargs')
+
+const command = argv._
+
+// console.log(process.argv)
+
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body)
+} else if (command === 'list') {
+  notes.listNote()
+} else if (command === 'read') {
+  notes.getNote(argv.title)
+} else if (command === 'remove') {
+  notes.removeNote(argv.title)
+} else {
+  console.log('Comando desconocido')
+}
--- notes.js
+++ notes.js
@@ -5,18 +5,29 @@ console.log('Modulo de notas cargado')
 //  return 'Nueva nota'
 // }
 
-const addNote = () => {
-  console.log('Nueva nota')
-  return 'Nueva nota'
+const addNote = (titulo, cuerpo) => {
+  console.log('Añado nota', titulo, cuerpo)
+  return 'Nueva nota añadida'
 }
 
-const deleteNote = (id) => {
-  console.log(`Nota  ${id} borrada`)
+const deleteNote = (titulo) => {
+  console.log(`Nota  ${titulo} borrada`)
   return 'Nota borrada'
+}
+
+const listNote = () => {
+  console.log('Te muestro todas las notas')
+  return 'RETAHILA'
+}
+const leerNote = (titulo) => {
+  console.log(titulo, `  dice : muuuuuua`)
+  return 'Nota leída'
 }
 
 module.exports = {
   addNote,
-  deleteNote
+  deleteNote,
+  listNote,
+  leerNote
 
 }
